7de1bf87aa618bcff3cc8872c3a91058f2a14c9f
张杰 <jwzhangjie@163.com>
2013-08-26 13:40:06 +0800

    添加web视频

---
+++ KeKePlayer/src/com/example/kekeplayer/activity/KeKeLive_Flash.java
@@ -0,0 +1,40 @@
+package com.example.kekeplayer.activity;
+
+import com.example.kekeplayer.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class KeKeLive_Flash extends Activity{
+
+	public WebView webView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.kekelive_flash);
+		webView = (WebView)findViewById(R.id.web_flash);
+		WebSettings settings = webView.getSettings();
+		settings.setPluginsEnabled(true);
+		settings.setJavaScriptEnabled(true);
+		settings.setAllowFileAccess(true);
+		webView.setBackgroundColor(0);
+		webView.setWebViewClient(new WebViewClient(){
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				webView.loadUrl(url);
+				return super.shouldOverrideUrlLoading(view, url);
+			}
+			
+		});
+		webView.setWebChromeClient(new WebChromeClient(){
+			
+		});
+		webView.loadUrl("http://www.baidu.com");
+	}
+
+}
